c418ff0d2705a5b11ce20a52795fd047d97f2c59
Ofir Shalev <ofirshalev1@gmail.com>
2014-12-02 22:38:22 +0800

    Include new var DateTime using as.POSIXct

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 ## plot1.R
 alldata <- read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings = c("?"),colClasses=c("character","character",rep("numeric",7)))
-alldata$Date <- strptime(alldata[,1], "%d/%m/%Y")
-rangedata <- alldata[alldata$Date == "2007-02-01" |  alldata$Date == "2007-02-02",]
+alldata$DateTime <- as.POSIXct(paste(alldata$Date, alldata$Time), format="%d/%m/%Y %H:%M:%S")
+rangedata <- alldata[alldata$DateTime >= "2007-02-01" & alldata$DateTime < "2007-02-03",]
 hist(rangedata$Global_active_power,main = "Global Active Power", col="red",xlab = "Global Active Power (kilowatts)")
 dev.copy(png, file = "plot1.png")
 dev.off()
