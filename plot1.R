0bc7e12890b77248402b26f82efa5ad70b7e3629
Ofir Shalev <ofirshalev1@gmail.com>
2014-12-02 21:22:56 +0800

    fixing Date issue and filtering

--- plot1.R
+++ plot1.R
@@ -1,6 +1,7 @@
-## alldata <- read.table("household_power_consumption.txt",sep=";",header=TRUE)
-alldata <- read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings = c("?"),colClasses=c("Date","character",rep("numeric",7)))
-rangedata <- alldata[data$Date == "1/2/2007" |  data$Date == "2/2/2007",]
+## plot1.R
+alldata <- read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings = c("?"),colClasses=c("character","character",rep("numeric",7)))
+alldata$Date <- strptime(alldata[,1], "%d/%m/%Y")
+rangedata <- alldata[alldata$Date == "2007-02-01" |  alldata$Date == "2007-02-02",]
 hist(rangedata$Global_active_power,main = "Global Active Power", col="red",xlab = "Global Active Power (kilowatts)")
 dev.copy(png, file = "plot1.png")
 dev.off()
